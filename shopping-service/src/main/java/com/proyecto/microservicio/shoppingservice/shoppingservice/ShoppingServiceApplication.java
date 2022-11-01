c189992391fd1a40c0b0e48fda17228764e769d3
matias.sanroman.32 <matias.sanroman.32@gmail.com>
2022-11-01 18:00:14 -0300

    add properties ms eureka client

--- shopping-service/src/main/java/com/proyecto/microservicio/shoppingservice/shoppingservice/ShoppingServiceApplication.java
+++ shopping-service/src/main/java/com/proyecto/microservicio/shoppingservice/shoppingservice/ShoppingServiceApplication.java
@@ -1,4 +1,4 @@
-package com.proyecto.microservicio.serviceproduct;
+package com.proyecto.microservicio.shoppingservice.shoppingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class ServiceProductApplication {
+public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceProductApplication.class, args);
+		SpringApplication.run(ShoppingServiceApplication.class, args);
 	}
 
 }
---
+++ shopping-service/src/test/java/com/proyecto/microservicio/shoppingservice/shoppingservice/ShoppingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicio.shoppingservice.shoppingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
